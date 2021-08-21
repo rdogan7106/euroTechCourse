a12cca0532ead76fd565efd4e3cbe3c0b7be0b22
rdogan7106 <78352219+rdogan7106@users.noreply.github.com>
2021-08-21 15:38:34 +0200

    drag and drop

--- src/Gun04/_04_DragAndDrop4.java
+++ src/Gun04/_04_DragAndDrop4.java
@@ -9,17 +9,26 @@ import org.openqa.selenium.interactions.Actions;
 public class _04_DragAndDrop4 extends BaseStaticDriver {
     public static void main(String[] args) {
         driver.get("https://talkerscode.com/webtricks/demo/demo_price-range-slider-using-jquery-css-and-php.php");
+        WebElement scrollWidth =driver.findElement(By.id("slider-range"));
         WebElement leftScroll = driver.findElement(By.xpath("(//span[@class='ui-slider-handle ui-state-default ui-corner-all'])[1]"));
         WebElement rightScroll = driver.findElement(By.xpath("(//span[@class='ui-slider-handle ui-state-default ui-corner-all'])[2]"));
 
         Actions actions = new Actions(driver);
         Action action1 = actions.dragAndDropBy(leftScroll,100,0).build();
         action1.perform();
-        Bekle(2);
 
         Actions actions2 =  new Actions(driver);
         Action action2 = actions2.dragAndDropBy(rightScroll,-100,0).build();
-        action2.perform();
+
+        int cubuk =  scrollWidth.getSize().width;
+        double oran = 500/cubuk;
+
+        Action actLeft  = actions.dragAndDropBy(leftScroll,(int)(200*oran),0).build();
+        actLeft.perform();
+        Action actRight = actions.dragAndDropBy(rightScroll,(int)(-100*oran),0).build();
+        actRight.perform();
+
         Bekle(2);
+        Kapat();
     }
 }
